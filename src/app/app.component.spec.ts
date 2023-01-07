d7acb368aed13f85a490a1ea8577f9981c510c30
SkOushikk <2000032025@kluniversity.in>
2023-01-07 10:42:00 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'koushik2000032025'`, () => {
+  it(`should have as title 'Space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('koushik2000032025');
+    expect(app.title).toEqual('Space');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('koushik2000032025 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Space app is running!');
   });
 });
